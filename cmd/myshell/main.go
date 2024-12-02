a5c5d55e06728ac5d9947202263fd5077193c137
Larry Botha <lawrenceguy@gmail.com>
2024-12-02 18:06:29 +0530

    feat(main.go): read input via loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -9,23 +9,25 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	reader, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err == io.EOF {
-		fmt.Println("closing shell...")
-	} else if err != nil {
-		fmt.Println("error: ", err.Error())
-	}
+		reader, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err == io.EOF {
+			fmt.Println("closing shell...")
+		} else if err != nil {
+			fmt.Println("error: ", err.Error())
+		}
 
-	inputs := strings.Split(strings.TrimSpace(reader), " ")
+		inputs := strings.Split(strings.TrimSpace(reader), " ")
 
-	if len(inputs) > 0 {
-		command := inputs[0]
+		if len(inputs) > 0 {
+			command := inputs[0]
 
-		switch command {
-		default:
-			fmt.Printf("%s: command not found\n", command)
+			switch command {
+			default:
+				fmt.Printf("%s: command not found\n", command)
+			}
 		}
 	}
 }
